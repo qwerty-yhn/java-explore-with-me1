b0f25717a7ee145347040620603c54f872db5cf1
Alexandr <qwerty-yhn@mail.ru>
2023-07-10 23:38:32 +0600

    Диплом 2 часть, первая итерация, почистил коменты блин

--- ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
+++ ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
@@ -2,9 +2,9 @@ package ru.practicum.explorewithme.stats.dto;
 
 import lombok.Builder;
 import lombok.Value;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
 
 @Value
 @Builder
