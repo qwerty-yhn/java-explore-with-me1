6e24cd42af6bab87e419eef2abc4ac77fb3a3723
Alexandr <qwerty-yhn@mail.ru>
2023-07-10 23:27:39 +0600

    Диплом 2 часть, первая итерация

--- ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
+++ ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
@@ -9,9 +9,14 @@ import javax.validation.constraints.Pattern;
 @Value
 @Builder
 public class HitDto {
+    
     Long id;
-    @NotBlank String app;
-    @NotBlank String uri;
-    @NotBlank String ip;
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", message = "Invalid date format") String timestamp;
+    @NotBlank
+    String app;
+    @NotBlank
+    String uri;
+    @NotBlank
+    String ip;
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", message = "Invalid date format")
+    String timestamp;
 }
