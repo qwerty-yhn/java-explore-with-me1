380beed22e0033316094ea11eb399ea5ace000d9
Alexandr <qwerty-yhn@mail.ru>
2023-07-13 01:13:17 +0600

    Диплом 2 часть, вторая итерация: 1. DATA_TIME теперь в утилитарном классе 2. Поправлен мавен сборка, 3. Откорректированы Exeptions 4. Откорректирована shema.sql

--- ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
+++ ewm-stat-service/stats-dto/src/main/java/ru/practicum/explorewithme/stats/dto/HitDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Value;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Value
 @Builder
